f5a40dd1453c2a58df80c90b2039b174fb0ca86d
Ryan Hendrawan <ryanhendrawan9@gmail.com>
2025-05-09 21:14:34 +0800

    fix: next.config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       "travel-journal-api-bootcamp.do.dibimbing.id",
     ],
   },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 };
 
 export default nextConfig;
